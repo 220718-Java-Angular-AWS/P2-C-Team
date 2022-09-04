ecdb95f59c78cd1428bfb29c65943d2f841cd6ca
Matthew Dufner <dufnerentertainment@gmail.com>
2022-09-04 11:34:20 -0500

    put a project structure in the folder for us to beginning merging our files

---
+++ MKPG/MKPG/src/main/java/com/revature/MKPG/MKPGApplication.java
@@ -0,0 +1,13 @@
+package com.revature.MKPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MKPGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MKPGApplication.class, args);
+	}
+
+}
---
+++ MKPG/MKPG/src/test/java/com/revature/MKPG/MKPGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.MKPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MKPGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
